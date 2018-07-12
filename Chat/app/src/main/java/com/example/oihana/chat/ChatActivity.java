e2815cc6a94482bee2d3830b090e016262cd97d6
violetahs <ti14110361@itess.edu.mx>
2018-07-12 14:58:26 -0500

    Ulizando el volley

--- Chat/app/src/main/java/com/example/oihana/chat/ChatActivity.java
+++ Chat/app/src/main/java/com/example/oihana/chat/ChatActivity.java
@@ -7,16 +7,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class ChatActivity extends AppCompatActivity {
 
     private EditText eTusuario;
     private EditText eTpassword;
     private Button bTingresar;
+    private RequestQueue mRequest;
+    private VolleyRP volley;
+    private static final String IP="https://judithhs.000webhostapp.com/ArchivosPHP/Login_GETID.php?id=";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        volley=VolleyRP.getInstance(this);
+        mRequest=volley.getRequestQueue();
         eTusuario=(EditText) findViewById(R.id.eTusuario);
         eTpassword=(EditText) findViewById(R.id.eTpaasword);
         bTingresar=(Button) findViewById(R.id.bTingresar);
@@ -36,5 +51,27 @@ public class ChatActivity extends AppCompatActivity {
 
     public void verificarLogin(String user,String pass){
         Toast.makeText(this, "El usuario es: "+user+"La contraseña es :"+pass, Toast.LENGTH_SHORT).show();
+        solicitudJSON(IP+user);
+    }
+    public void solicitudJSON(String URL){
+        JsonObjectRequest solicitud=new JsonObjectRequest(URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject datos) {
+                verificarLogin(datos);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ChatActivity.this, "Ocurrio un error por favoy comuniquese como el administrador", Toast.LENGTH_SHORT).show();
+            }
+        });
+        VolleyRP.addToQueue(solicitud,mRequest,this,volley);
+    }
+
+    public void verificarLogin(JSONObject datos){
+        //Controlar el JSON
+
+
     }
 }
